84fd4043c901be38371fd888ed8513846e282221
Asfaque Ali Ansari <asfaquealiansari@gmail.com>
2019-12-12 17:43:48 +0530

    Updates

---
+++ precache-manifest.aaa3539dda076709a8427195a2de818e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f237278d034919a5e74780021bae0a20",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "7ecd414d8e7591c6b72a",
+    "url": "/todo-app/static/css/main.e300657f.chunk.css"
+  },
+  {
+    "revision": "95b63b230333112aec9a",
+    "url": "/todo-app/static/js/2.0ee00a21.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/todo-app/static/js/2.0ee00a21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ecd414d8e7591c6b72a",
+    "url": "/todo-app/static/js/main.72d2a88e.chunk.js"
+  },
+  {
+    "revision": "04c0e12a0dd748a4bb99",
+    "url": "/todo-app/static/js/runtime-main.a5594779.js"
+  }
+]);
